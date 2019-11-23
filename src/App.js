ee98dc936987a65f3cfe093eb71463ad68a1a6d7
patomation <patomation@gmail.com>
2019-11-23 12:51:20 +0700

    using stand alone keyboard component lib instead of react-ui-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { Keyboard, Grid } from 'react-ui-components';
-
+import { Center } from '@patomation/react-ui-components';
+import Keyboard from '@patomation/react-keyboard'
 import audioPlayers from './audioPlayers.js';
 
 
@@ -9,33 +9,38 @@ const App = () => {
   return (
     <div className='app'>
 
+      <Center>
         <h2  style={{textAlign: 'center'}}>{"K E Y B O A R D D R U M S"}</h2>
 
-      <Keyboard
-        onDown={ key => {
-          console.log('key down', key);
-          if(audioPlayers[key]) audioPlayers[key].play()
-        }}
-        onUp={ key => {
-          console.log('key up', key);
-          if(audioPlayers[key]) audioPlayers[key].pause()
-        }}
-        buttonStyle={{
-          color: '#49393b',
-          background: '#89a894'
-        }}
-        hoverStyle={{
-          background: '#4b644a'
-        }}
-        activeStyle={{
-          color: '#49393b',
-          background: '#adfcf9'
-        }}
-        disabledStyle={{
-          background: '#4b644a'
-        }}
-         />
-
+        <Keyboard
+          style={{
+            maxWidth: '600px',
+            margin: '0 auto'
+          }}
+          onDown={ key => {
+            console.log('key down', key);
+            if(audioPlayers[key]) audioPlayers[key].play()
+          }}
+          onUp={ key => {
+            console.log('key up', key);
+            if(audioPlayers[key]) audioPlayers[key].pause()
+          }}
+          buttonStyle={{
+            color: '#49393b',
+            background: '#89a894'
+          }}
+          hoverStyle={{
+            background: '#4b644a'
+          }}
+          activeStyle={{
+            color: '#49393b',
+            background: '#adfcf9'
+          }}
+          disabledStyle={{
+            background: '#4b644a'
+          }}
+           />
+        </Center>
     </div>
   );
 }
